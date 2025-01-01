f447b6d52e1c207e28ba4c57f7207894d58eb50e
Kiel Lofstrand <kiel.lofstrand@gmail.com>
2025-01-01 10:13:57 -0500

    Created ExtronSw4Hd4k class

---
+++ src/AV/ExtronSw4Hd4k.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Crestron.SimplSharp;
+
+namespace HomeOfficeControl.AV
+{
+    public class ExtronSw4Hd4k : ExtronVideoSwitcher
+    {
+        public ExtronSw4Hd4k() : base(4, 1)
+        {
+
+        }
+    }
+}
