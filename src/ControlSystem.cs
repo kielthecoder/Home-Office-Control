5af6b28e5f460e08b3e86c2cc493dbdc3d2f86e7
Kiel Lofstrand <kiel.lofstrand@gmail.com>
2025-01-02 08:28:18 -0500

    Added SGD file to project build

--- src/ControlSystem.cs
+++ src/ControlSystem.cs
@@ -11,12 +11,14 @@ using Crestron.SimplSharpPro.GeneralIO;
 using Crestron.SimplSharpPro.UI;
 
 using HomeOfficeControl.AV;
+using HomeOfficeControl.UI;
 
 namespace HomeOfficeControl
 {
     public class ControlSystem : CrestronControlSystem
     {
         private ExtronSw4Hd4k[] _switcher;
+        private OfficeUI _ui;
 
         public ControlSystem()
         {
@@ -31,17 +33,8 @@ namespace HomeOfficeControl
 
         public override void InitializeSystem()
         {
-            var tsw = new Ts770(0x03, this); // TS-770 is the main interface
-
-            tsw.ExtenderTouchDetectionReservedSigs.Use();
-            tsw.ExtenderTouchDetectionReservedSigs.Time.UShortValue = 120; // 2 minutes
-            tsw.ExtenderTouchDetectionReservedSigs.DeviceExtenderSigChange += _tsw_TouchDetection;
-
-            tsw.Register();
-
             var exp = new CenIoCom102(0xA5, this); // CEN-IO-COM-102 is used for HDMI switchers
             exp.OnlineStatusChange += _exp_OnlineStatusChange;
-
             exp.Register();
 
             _switcher[0] = new ExtronSw4Hd4k(); // Switcher for Primary monitor
@@ -49,12 +42,34 @@ namespace HomeOfficeControl
 
             _switcher[1] = new ExtronSw4Hd4k(); // Switcher for Secondary monitor
             _switcher[1].UseSerial(exp.ComPorts[2]);
+
+            var tsw = new Ts770(0x03, this); // TS-770 is the main interface
+
+            tsw.ExtenderTouchDetectionReservedSigs.Use();
+            tsw.ExtenderTouchDetectionReservedSigs.Time.UShortValue = 120; // 2 minutes
+            tsw.ExtenderTouchDetectionReservedSigs.DeviceExtenderSigChange += _tsw_TouchDetection;
+
+            InitializeUI(tsw);
+        }
+
+        void InitializeUI(BasicTriListWithSmartObject tsw)
+        {
+            CrestronConsole.PrintLine("InitializeUI: {0}", tsw);
+
+            _ui = new OfficeUI(tsw, "Home Office_TS-770_v1.sgd");
+
+            var uiSw1 = new VideoSwitcherControl(_ui, _switcher[0], new uint[] { 50, 51, 52, 53, 54 }); // Primary monitor joins
+            var uiSw2 = new VideoSwitcherControl(_ui, _switcher[1], new uint[] { 60, 61, 62, 63, 64 }); // Secondary monitor joins
+
+            tsw.Register();
         }
 
         void OnProgramStatusChange(eProgramStatusEventType type)
         {
             if (type == eProgramStatusEventType.Stopping) // Clean things up before the program stops
             {
+                _ui.Dispose();
+
                 _switcher[0].Dispose();
                 _switcher[1].Dispose();
 
@@ -70,7 +85,7 @@ namespace HomeOfficeControl
                 {
                     if (args.Sig.BoolValue == false) // No activity detected
                     {
-                        // TODO: go back to clock activity
+                        _ui.SelectActivity(Activity.Clock);
                     }
                 }
             }
@@ -80,11 +95,8 @@ namespace HomeOfficeControl
         {
             if (args.DeviceOnLine)
             {
-                // TODO: re-enable polling?
-            }
-            else
-            {
-                // TODO: pause polling?
+                // Default to Clock activity
+                _ui.SelectActivity(Activity.Clock);
             }
         }
     }
